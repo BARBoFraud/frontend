f52e7524f7fe43b83530d7328fa798a4d0d78dad
Jorge Cadena <a01660252@tec.mx>
2025-09-11 17:54:25 -0600

    First commit

---
+++ BARBoFraud/BARBoFraudApp.swift
@@ -0,0 +1,17 @@
+//
+//  BARBoFraudApp.swift
+//  BARBoFraud
+//
+//  Created by Jorge Cadena on 11/09/25.
+//
+
+import SwiftUI
+
+@main
+struct BARBoFraudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
