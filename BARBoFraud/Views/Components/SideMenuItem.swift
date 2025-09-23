a9d6c209297b700def0dbd5d43e535012a8107d1
BarbieMunoz <a01659348@tec.mx>
2025-09-23 15:48:51 -0600

    components used for the side menu

---
+++ BARBoFraud/Views/Components/SideMenuItem.swift
@@ -0,0 +1,18 @@
+//
+//  SideMenuItem.swift
+//  BARBoFraud
+//
+//  Created by Usuario on 23/09/25.
+//
+
+import SwiftUI
+
+struct SideMenuItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SideMenuItem()
+}
