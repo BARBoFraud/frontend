e201b752f40af18b17f3c8e4b4f12b1c6e77c4d9
BarbieMunoz <a01659348@tec.mx>
2025-09-23 14:25:17 -0600

    DTO for article in the info screen

---
+++ BARBoFraud/DTOs/ArticleDTO.swift
@@ -0,0 +1,18 @@
+//
+//  ArticleDTO.swift
+//  BARBoFraud
+//
+//  Created by Usuario on 23/09/25.
+//
+
+import SwiftUI
+
+struct ArticleDTO: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ArticleDTO()
+}
