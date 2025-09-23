b3aadda0e4dcb2debbe96e4373cccf204e695738
BarbieMunoz <a01659348@tec.mx>
2025-09-23 14:35:02 -0600

    it can now navigate to the article detail view when the user clicks on the button

---
+++ BARBoFraud/Views/Components/ArticleRow.swift
@@ -0,0 +1,27 @@
+//
+//  ArticleRow.swift
+//  BARBoFraud
+//
+//  Created by Barbie on 23/09/25.
+//
+
+import SwiftUI
+struct ArticleRow: View {
+    let article: ArticleDTO
+    @State private var navigate = false
+    
+    var body: some View {
+        ZStack {
+            ArticleCardView(article: article) {
+                navigate = true
+            }
+            NavigationLink(destination: ArticleDetailView(article: article), isActive: $navigate) {
+                EmptyView()
+            }
+            .hidden()
+        }
+    }
+}
+
+
+
