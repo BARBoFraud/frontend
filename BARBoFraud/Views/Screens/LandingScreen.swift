41fadd41ed8262dd796dc5a7b66b4db4542bdaff
BarbieMunoz <a01659348@tec.mx>
2025-09-22 19:16:26 -0600

    Changed the landing screen so that the buttons can change screens and so that it uses the backgroundView component

--- BARBoFraud/Views/Screens/LandingScreen.swift
+++ BARBoFraud/Views/Screens/LandingScreen.swift
@@ -9,58 +9,43 @@ import SwiftUI
 
 struct LandingScreen: View {
     var body: some View {
-        ZStack {
-            // Background gradient color
-            LinearGradient(colors: [Color.landingBg2, Color.landingBg1],
-                           startPoint: .top,
-                           endPoint: .bottom)
-            .ignoresSafeArea()
-            // Background waves
-            LandingWaves()
-            
-            VStack {
-            Spacer()
-            Text("oFraud")
-                .font(.largeTitle)
-                .foregroundColor(.primary)
-                .padding(30)
-                .frame(width: 300)
-                .background(.landingBg2)
-                .cornerRadius(15)
-                    
-            Spacer()
-            
-            // Buttons HStack
-            HStack(spacing: 20) {
-                // Login Button
-                Button(action: {
-                    print("Login")
-                }) {
-                    Text("Login")
+        NavigationStack {
+            BackgroundView {
+                VStack {
+                    Spacer()
+                    Text("oFraud")
+                        .font(.largeTitle.bold())
                         .foregroundColor(.white)
-                        .font(.headline)
-                        .foregroundColor(.primary)
-                        .padding()
-                        .frame(maxWidth: .infinity)
-                        .background(.blueAccent)
-                        .cornerRadius(12)
-                }
+                        .padding(30)
+                        .frame(width: 300)
+                        .background(Color("Waves"))
+                        .cornerRadius(15)
+                    
+                    Spacer()
+                    HStack(spacing: 20) {
+                        NavigationLink(destination: LogIn().navigationBarBackButtonHidden(true)) {
+                            Text("Iniciar Sesión")
+                                .font(.headline)
+                                .foregroundColor(.white)
+                                .padding()
+                                .frame(maxWidth: .infinity)
+                                .background(Color("BlueAccent"))
+                                .cornerRadius(12)
+                        }
                         
-                // Signup Button
-                Button(action: {
-                    print("Signup")
-                }) {
-                    Text("Signup")
-                        .font(.headline)
-                        .foregroundColor(.primary)
-                        .padding()
-                        .frame(maxWidth: .infinity)
-                        .background(.landingBg2)
-                        .cornerRadius(12)
+                        NavigationLink(destination: SignUp().navigationBarBackButtonHidden(true)) {
+                            Text("Registrarse")
+                                .font(.headline)
+                                .foregroundColor(.white)
+                                .padding()
+                                .frame(maxWidth: .infinity)
+                                .background(Color("Waves"))
+                                .cornerRadius(12)
+                        }
+                    }
+                    .padding(.horizontal, 30)
+                    .padding(.bottom, 50)
                 }
-            }
-            .padding(.horizontal, 30)
-            .padding(.bottom, 50)
             }
         }
     }
