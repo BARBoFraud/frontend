885821caea278e1ba18b1c20908bea8e285ce7b7
BarbieMunoz <a01659348@tec.mx>
2025-09-23 14:25:54 -0600

    article DTO for the info screen

--- BARBoFraud/DTOs/ArticleDTO.swift
+++ BARBoFraud/DTOs/ArticleDTO.swift
@@ -7,12 +7,28 @@
 
 import SwiftUI
 
-struct ArticleDTO: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+struct ArticleDTO: Identifiable {
+    let id = UUID()
+    let title: String
+    let description: String
+    let imageName: String?
 }
+let sampleArticles: [ArticleDTO] = [
+    ArticleDTO(
+        title: "La importancia de la educación tecnológica",
+        description: """
+        Actualmente la tecnología ha llegado para quedarse y somos testigos de todos los avances que ha tenido, para mejorar nuestra vida cotidiana, y con la llegada delSARS-CoV-2, revolucionó por completo el uso de estas herramientas y en el ámbito de la educación no fue la excepción.
+        """,
+        imageName: "art1"
+    ),
+    ArticleDTO(
+        title: "¿A cuánto se venden los datos personales en internet?",
+        description: """
+        ¿Sabías que el compartir una foto en redes sociales hasta el navegar en la web, deja un rastro de información personal? Pues a esto se le conoce como huella digital y mientras más cuentas en línea tenga un usuario o más navegue en internet, su huella digital aumenta.
+        """,
+        imageName: "art2"
+    )
+]
 
-#Preview {
-    ArticleDTO()
-}
+
+
