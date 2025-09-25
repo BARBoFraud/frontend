7ff0e3f16dc8f82ef470fc9f7960669cefcf42c3
Jorge Cadena <a01660252@tec.mx>
2025-09-25 17:06:35 -0600

    Created skelleton function

---
+++ BARBoFraud/ViewModels/Login/ValidateLogin.swift
@@ -0,0 +1,15 @@
+//
+//  ValidateLogin.swift
+//  BARBoFraud
+//
+//  Created by Jorge Cadena on 24/09/25.
+//
+
+// TODO: Pass the user login DTO as a parameter
+//
+
+func ValidateLogin() -> Bool {
+    // Validation logic
+    
+    return true
+}
