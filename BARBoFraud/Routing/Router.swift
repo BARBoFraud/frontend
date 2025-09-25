2640a4e43fb2d18e4d93169384331b1927c1be7b
Jorge Cadena <a01660252@tec.mx>
2025-09-25 17:59:50 -0600

    Modified the routing method to be able to not repeat screens

--- BARBoFraud/Routing/Router.swift
+++ BARBoFraud/Routing/Router.swift
@@ -9,17 +9,26 @@ import Foundation
 
 final class Router : ObservableObject {
     @Published var path = NavigationPath()
+    private var routes: [Route] = []
     
     func push(_ route: Route) {
-        path.append(route)
+        if (routes.last != route) {
+            routes.append(route)
+            path.append(route)
+        }
+        print("\n\n\n\nCurrent path: \(path)")
     }
     
     func reset(to route: Route) {
+        routes = [route]
         path = NavigationPath([route])
+        print("\n\n\n\nCurrent path: \(path)")
     }
     
     func pop() {
+        routes.removeLast()
         path.removeLast()
+        print("\n\n\n\nCurrent path: \(path)")
     }
     
     func clear() {
