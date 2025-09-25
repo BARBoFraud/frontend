af0cc54858442e6ae3a0b859b3153662f90696f0
BarbieMunoz <a01659348@tec.mx>
2025-09-25 16:24:20 -0600

    created the user profile dto

---
+++ BARBoFraud/Networking/HTTPClient.swift
@@ -0,0 +1,48 @@
+//
+//  HTTPClient.swift
+//  RegistroUsuario452
+//
+//  Created by Barbie on 29/08/25.
+//
+
+import Foundation
+
+struct HTTPClient {
+    
+    func UserRegistration(name:String, lastName1:String, lastName2:String,email:String, password:String) async throws {
+        let requestForm = RegistrationFormRequest(name:name, lastName1: lastName1, lastName2: lastName2,email:email, password:password)
+        let url = URL(string: "http://10.48.234.212:3000/v1/users/register")!
+        var httpRequest = URLRequest(url: url)
+        httpRequest.httpMethod = "POST"
+        httpRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        let jsonData = try JSONEncoder().encode(requestForm)
+        httpRequest.httpBody = jsonData
+        
+        let (_, response) = try await URLSession.shared.data(for: httpRequest)
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw URLError(.badServerResponse)
+        }
+        
+        guard 200..<300 ~= httpResponse.statusCode else {
+            throw URLError(.badServerResponse)
+        }
+        
+        print("Usuario registrado correctamente")
+    }
+    func UserLogin(email:String, password:String) async throws -> LoginResponse{
+        let loginRequest = LoginRequest(email:email, password:password)
+        //let url = URL(string: "http://localhost:3000/auth/login")!
+        guard let url = URL(string: "http://10.48.234.212:3000/v1/auth/users/login") else {
+            fatalError("Invalid URL" + "http://localhost:3000/auth/users/login")
+        }
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "POST"
+        urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        urlRequest.httpBody = try? JSONEncoder().encode(loginRequest)
+        let (data, response) = try await URLSession.shared.data(for:urlRequest)
+        let loginResponse = try JSONDecoder().decode(LoginResponse.self, from: data)
+        return loginResponse
+    }
+    
+    
+}
---
+++ BARBoFraud/Storage/TokenStorage.swift
@@ -0,0 +1,30 @@
+//
+//  TokenStorage.swift
+//  RegistroUsuario452
+//
+//  Created by Barbie on 05/09/25.
+//
+
+import Foundation
+
+struct TokenStorage {
+    @discardableResult
+    static func set(identifier: String, value: String) -> Bool {
+        UserDefaults.standard.set(value, forKey: identifier)
+        return true
+    }
+    
+    static func get(identifier: String) -> String? {
+        UserDefaults.standard.string(forKey: identifier)
+    }
+    
+    @discardableResult
+    static func delete(identifier: String) -> Bool {
+        let existed = UserDefaults.standard.object(forKey: identifier) != nil
+        if existed {
+            UserDefaults.standard.removeObject(forKey: identifier)
+        }
+        return existed
+    }
+}
+
