03961ebd8f2df751e563a2da261e0a13a801983c
doyDynamo <dho2000@hotmail.com>
2025-09-25 18:20:35 -0600

    Update PostDTO

--- BARBoFraud/DTOs/PostDTO.swift
+++ BARBoFraud/DTOs/PostDTO.swift
@@ -11,16 +11,20 @@ struct PostResponse : Decodable {
     let post : Post
 }
 
-struct Post : Decodable {
+struct Post : Decodable, Identifiable {
+    let id : Int
     let firstName, lastName, title, postText : String
     let image : Data?
     let date : Date
+    let likeCount, commentCount : Int
     
     enum CodingKeys: String, CodingKey {
+        case id
         case firstName = "first_name"
         case lastName = "last_name"
         case postText = "post_text"
         case title, image, date
-        
+        case likeCount = "like_count"
+        case commentCount = "comment_count"
     }
 }
