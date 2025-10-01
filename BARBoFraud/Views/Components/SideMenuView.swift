b31a3351127c2794fc35160b3fe8f6ca7eea05dd
BarbieMunoz <a01659348@tec.mx>
2025-10-01 16:46:49 -0600

    implemente funcion temporal para cerra sesion (sin endpoint) y empece a hacer la funcionalidad de llamar a la policia

--- BARBoFraud/Views/Components/SideMenuView.swift
+++ BARBoFraud/Views/Components/SideMenuView.swift
@@ -16,8 +16,15 @@ struct SideMenuView: View {
     @State private var lastName1: String = ""
     @State private var email: String = ""
     
+    @State private var navLanging: Bool = false
+    
     @State private var showingLogoutCard = false
     @Environment(\.authController) var authController
+    @AppStorage("isLoggedIn") var isLoggedIn: Bool = true
+    
+    @Environment(\.openURL) var openUrl
+    @State private var showingPoliceCard: Bool = false
+    let policeNumber = 5611773689
     
     private func loadProfile() async {
         do {
@@ -30,60 +37,84 @@ struct SideMenuView: View {
         } catch {
             print("Error al cargar el perfil: \(error)")
         }
-        
+    }
+    
+    private func callPolice() async {
+        guard let url = URL(string: "tel://\(policeNumber)") else { return }
+    }
+    
+    private func logOutBtnTapped() async {
+        await authController.logout()
+        isLoggedIn = false
+        navLanging = true
     }
     
     var body: some View {
-            VStack(alignment: .leading, spacing: 20) {
-                Spacer().frame(height: 30)
-                VStack(alignment: .leading, spacing: 2) {
-                    HStack {
-                        Text(name)
-                            .font(.title2)
-                            .foregroundColor(.text)
-                        Text("\(lastName1) \(lastName2)")
-                            .font(.title2)
-                            .foregroundColor(.text.opacity(0.7))
-                    }
-                    
-                    Text(verbatim: "\(email)")
-                        .foregroundColor(.text.opacity(0.5))
-                        .font(.title3)
+        NavigationStack {
+            ZStack {
+                VStack(alignment: .leading, spacing: 20) {
+                    Spacer().frame(height: 30)
+                    VStack(alignment: .leading, spacing: 2) {
+                        HStack {
+                            Text(name)
+                                .font(.title2)
+                                .foregroundColor(.text)
+                            Text("\(lastName1) \(lastName2)")
+                                .font(.title2)
+                                .foregroundColor(.text.opacity(0.7))
+                        }
                         
+                        Text(verbatim: "\(email)")
+                            .foregroundColor(.text.opacity(0.5))
+                            .font(.title3)
+                        
+                    }
+                    .padding(.top, 40)
+                    .padding(.horizontal)
+                    
+                    Rectangle()
+                        .fill(.black.opacity(0.7))
+                        .frame(height: 2, alignment: .leading)
+                    
+                    
+                    Group {
+                        SideMenuItem(icon: "clock.arrow.circlepath", label: "Historial de reportes") {}
+                        SideMenuItem(icon: "gearshape.fill", label: "Configuraciones") {}
+                    }
+                    .padding(.horizontal)
+                    
+                    Rectangle()
+                        .fill(.black.opacity(0.7))
+                        .frame(height: 2, alignment: .leading)
+                    
+                    
+                    Group {
+                        SideMenuItem(icon: "phone.fill", label: "Contacto ciber policía") {}
+                        SideMenuItem(icon: "iphone.and.arrow.forward", label: "Cerrar sesión") {
+                            showingLogoutCard = true
+                        }
+                    }
+                    .padding(.horizontal)
+                    
+                    Spacer()
+                    
                 }
-                .padding(.top, 40)
-                .padding(.horizontal)
-
-                Rectangle()
-                    .fill(.black.opacity(0.7))
-                    .frame(height: 2, alignment: .leading)
-
+                .frame(width: 300, height: UIScreen.main.bounds.height, alignment: .leading)
+                .background(.appBg)
+                .edgesIgnoringSafeArea(.vertical)
                 
-                Group {
-                    SideMenuItem(icon: "clock.arrow.circlepath", label: "Historial de reportes") {}
-                    SideMenuItem(icon: "gearshape.fill", label: "Configuraciones") {}
+                if showingLogoutCard {
+                    ConfirmationCard(isPresented: $showingLogoutCard, title: "¿Seguro que deseas cerrar sesión?", confirmAction: logOutBtnTapped)
                 }
-                .padding(.horizontal)
-                
-                Rectangle()
-                    .fill(.black.opacity(0.7))
-                    .frame(height: 2, alignment: .leading)
-
-                
-                Group {
-                    SideMenuItem(icon: "phone.fill", label: "Contacto ciber policía") {}
-                    SideMenuItem(icon: "iphone.and.arrow.forward", label: "Cerrar sesión") {}
+                NavigationLink(destination: LandingScreen().navigationBarBackButtonHidden(true), isActive: $navLanging) {
+                    EmptyView()
                 }
-                .padding(.horizontal)
-                
-                Spacer()
+
             }
-            .frame(width: 300, height: UIScreen.main.bounds.height, alignment: .leading)
-            .background(.appBg)
-            .edgesIgnoringSafeArea(.vertical)
             .task {
                 await loadProfile()
             }
+        }
     }
 }
 
