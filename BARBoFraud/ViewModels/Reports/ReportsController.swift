05c66152800e646e7fd9dc9bb19be5a8ab3452ae
Daniel Esteban <danyjudoka93@gmail.com>
2025-10-09 21:09:15 -0600

    Cree el controlador de los reportes

--- BARBoFraud/ViewModels/Reports/ReportsController.swift
+++ BARBoFraud/ViewModels/Reports/ReportsController.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+struct ReportsController {
+    let httpClient: HTTPClientReports
+    func publishAReport(description: String, url: String?, website: String?, socialMedia: String?, phoneNumber: String?, userName: String?, email: String?, anonymous: Bool, imageId: String) async throws {
+        try await httpClient.PublishReport(description:description, url:url, website: website, socialMedia: socialMedia, phoneNumber: phoneNumber, userName: userName, email: email, anonymous: anonymous, imageId: imageId)
+    }
+    
+    
+}
