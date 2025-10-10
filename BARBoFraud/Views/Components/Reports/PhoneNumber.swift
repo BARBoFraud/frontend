686572c28db0c48210fb939a2bd46dfe9718125d
Daniel Esteban <danyjudoka93@gmail.com>
2025-10-10 09:55:58 -0600

    Cree el componente para leer el numero de telefono

--- BARBoFraud/Views/Components/Reports/PhoneNumber.swift
+++ BARBoFraud/Views/Components/Reports/PhoneNumber.swift
@@ -8,11 +8,56 @@
 import SwiftUI
 
 struct PhoneNumber: View {
+    @State private var expanded: Bool = true
+    @Binding var phoneNumber: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 4) {
+            HStack {
+                Button(action: {
+                    withAnimation{
+                        expanded.toggle()
+                    }
+                }) {
+                    Image(systemName: expanded ? "chevron.right" : "chevron.down")
+                        .font(.title2)
+                }
+                .foregroundColor(.text)
+                Spacer()
+                Text("Número telefónico")
+                    .font(.title2)
+                    .bold()
+                    .padding(.bottom, 8)
+                    .padding(.horizontal, 20)
+                Spacer()
+            }
+            
+            if expanded {
+                TextField("Número de 10 dígitos", text: $phoneNumber)
+                    .font(.system(size: 20, weight: .medium))
+                    .padding(.horizontal, 12)
+                    .background(Color.white.opacity(0.3))
+                    .cornerRadius(10)
+                    .padding(.horizontal, 0.1)
+            }
+        }
+        .padding()
+        .background(
+            RoundedRectangle(cornerRadius: 19)
+                .stroke(Color.blue, lineWidth: 3)
+                .frame(width: 300)
+        )
+        .frame(width: 300)
     }
 }
 
 #Preview {
-    PhoneNumber()
+    struct prev: View {
+        @State var phoneNumber: String = ""
+        
+        var body: some View {
+            PhoneNumber(phoneNumber: $phoneNumber)
+            }
+        }
+    return prev()
 }
