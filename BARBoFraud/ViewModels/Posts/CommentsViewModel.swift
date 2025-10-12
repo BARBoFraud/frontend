5b3551a7344d6e81514ef2ceb35a099406b491eb
doyDynamo <dho2000@hotmail.com>
2025-10-12 13:16:20 -0600

    Updated the view models in order to manage requests using Network Manager

--- BARBoFraud/ViewModels/Posts/CommentsViewModel.swift
+++ BARBoFraud/ViewModels/Posts/CommentsViewModel.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+@MainActor
+final class CommentsViewModel: ObservableObject {
+    
+    @Published var comments: Comments = []
+    @Published var isLoading = true
+    @Published var errorMessage: String?
+
+    func comment(id reportID: Int, text: String) async {
+        do{
+            let _ = try await NetworkManager.shared.comment(id: reportID, text: text)
+        } catch {
+            print("No se pudo comentar el reporte \(error.localizedDescription)")
+        }
+    }
+    
+    func getComments(id reportID: Int) async throws {
+        isLoading = true
+        do{
+             comments = try await NetworkManager.shared.getComments(id: reportID)
+        } catch {
+            print("No se pudierosn obtener los comentarios \(error.localizedDescription)")
+            throw error
+        }
+        isLoading = false
+    }
+}
--- BARBoFraud/ViewModels/Posts/FeedViewModel.swift
+++ BARBoFraud/ViewModels/Posts/FeedViewModel.swift
@@ -10,35 +10,34 @@ import Foundation
 @MainActor
 final class FeedViewModel: ObservableObject {
     
-    private let baseUrl = AppConfig.apiBaseUrl
-    
     @Published var posts: Feed = []
     @Published var isLoading = true
     @Published var errorMessage: String?
 
-    private lazy var endpoint = URL(string: "\(baseUrl)/reports/feed")!
-    //private let endpoint = URL(string: "http://192.168.1.79:4000/v1/reports/feed")! //iphone
-    func fetch() async {
+    func fetch() async throws {
         isLoading = true
         errorMessage = nil
         do {
-            guard let token = TokenStorage.get(identifier: "accessToken"), !token.isEmpty else {
-                throw NetworkError.noToken
-            }
-            
-            var httpRequest = URLRequest(url: endpoint)
-            httpRequest.httpMethod = "GET"
-            httpRequest.addValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
-            
-            let (data, response) = try await URLSession.shared.data(for: httpRequest)
-            guard let http = response as? HTTPURLResponse, (200...299).contains(http.statusCode) else {
-                throw URLError(.badServerResponse)
-            }
-            let decoded = try JSONDecoder().decode(Feed.self, from: data)
-            posts = decoded
+            posts = try await NetworkManager.shared.getFeed()
         } catch {
             errorMessage = "No se pudieron cargar las incidencias. \(error.localizedDescription)"
         }
         isLoading = false
+    }
+    
+    func like(id postID: Int) async throws {
+        do{
+            let _ = try await NetworkManager.shared.like(postID: postID)
+        } catch {
+            errorMessage = "No se pudo likear \(error.localizedDescription)"
+        }
+    }
+    
+    func unlike(id postID: Int) async throws {
+        do{
+            let _ = try await NetworkManager.shared.like(postID: postID)
+        } catch {
+            print("No se pudo quitar el like \(error.localizedDescription)")
+        }
     }
 }
