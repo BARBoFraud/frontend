5c06c395116dc3c1238f73b1797e47bd555e3aff
Jorge Cadena <a01660252@tec.mx>
2025-10-13 17:58:13 -0600

    Added Search results view inside the Search screen

--- BARBoFraud/Views/Components/Search/SearchResult.swift
+++ BARBoFraud/Views/Components/Search/SearchResult.swift
@@ -8,11 +8,64 @@
 import SwiftUI
 
 struct SearchResult: View {
+    @ObservedObject var vm: SearchViewModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if vm.query.isEmpty {
+            EmptyView()
+        } else {
+            if vm.isLoading {
+                Spacer()
+                ProgressView("Buscando")
+                    .progressViewStyle(CircularProgressViewStyle())
+                    .padding()
+                Spacer()
+            } else if vm.results.isEmpty {
+                Divider()
+                ZStack {
+                    Rectangle()
+                        .fill(.appBg)
+                        .shadow(color: .black.opacity(0.1), radius: 1, y: 1)
+                    HStack {
+                        Text("No se encontró ningún resultado")
+                    }
+                    .padding(.vertical, 10)
+                }
+                .fixedSize(horizontal: false, vertical: true)
+            } else {
+                ScrollView {
+                    LazyVStack(spacing: 0) {
+                        ForEach(vm.results) { result in
+                            Divider()
+                            ZStack {
+                                Rectangle()
+                                    .fill(.appBg)
+                                    .shadow(color: .black.opacity(0.1), radius: 1, y: 1)
+                                HStack {
+                                    if let website = result.website {
+                                        Text(website)
+                                    }
+                                    if let email = result.email {
+                                        Text(email)
+                                    }
+                                    if let phone = result.phoneNumber {
+                                        Text(phone)
+                                    }
+                                    if let social = result.socialMedia {
+                                        Text(social)
+                                    }
+                                }
+                                .padding(.vertical, 10)
+                            } // ZStack
+                            .fixedSize(horizontal: false, vertical: true)
+                        } // ForEach
+                    } // LazyVStack
+                } // ScrollView
+            } // else
+        }
     }
 }
 
 #Preview {
-    SearchResult()
+    SearchResult(vm: SearchViewModel())
 }
