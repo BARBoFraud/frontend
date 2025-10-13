fa3744a440cb6c68385896f6427333954d34140c
Jorge Cadena <a01660252@tec.mx>
2025-10-13 17:57:44 -0600

    Created ViewModel to Search

---
+++ BARBoFraud/ViewModels/Search/SearchViewModel.swift
@@ -0,0 +1,40 @@
+//
+//  SearchViewModel.swift
+//  BARBoFraud
+//
+//  Created by Jorge Cadena on 12/10/25.
+//
+
+import Foundation
+
+@MainActor
+final class SearchViewModel : ObservableObject {
+    @Published var query: String = ""
+    @Published var results: [SearchResponse] = []
+    @Published var isLoading = false
+    
+    func search(query: String) async {
+        isLoading = true
+        
+        await MainActor.run { self.query = query }
+        
+        // Don't make API call if text is empty
+        guard !query.trimmingCharacters(in: .whitespaces).isEmpty else {
+            await MainActor.run { self.results = [] }
+            return
+        }
+        
+        await MainActor.run { self.isLoading = true }
+        defer { Task { @MainActor in self.isLoading = false } }
+        
+        do {
+            let fetched = try await NetworkManager.shared.search(query: query)
+            await MainActor.run {
+                self.results = fetched
+            }
+        } catch {
+           print("Error al hacer el request al servidor: \(error)")
+        }
+        isLoading = false
+    }
+}
---
+++ BARBoFraud/Views/Components/Search/SearchResult.swift
@@ -0,0 +1,18 @@
+//
+//  SearchResult.swift
+//  BARBoFraud
+//
+//  Created by Jorge Cadena on 12/10/25.
+//
+
+import SwiftUI
+
+struct SearchResult: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchResult()
+}
