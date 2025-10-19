d77505a5a0a48245e0499f8c82736845a53cdf78
Jorge Cadena <a01660252@tec.mx>
2025-10-19 00:53:29 -0600

    Created History API calls

--- BARBoFraud/ViewModels/History/HistoryViewModel.swift
+++ BARBoFraud/ViewModels/History/HistoryViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import SwiftUI
 
 @MainActor
 final class HistoryViewModel : ObservableObject {
@@ -22,6 +23,27 @@ final class HistoryViewModel : ObservableObject {
             }
         } catch {
             print("Error al solicitar el historial: \(error.localizedDescription)")
+        }
+        isLoading = false
+    }
+    
+    func getCategoryId(_ category: String) async -> Int {
+        do {
+            let fetched = try await NetworkManager.shared.getCategoryId(category)
+            return fetched.id
+        } catch {
+            print("Error al solicitar el ID de la categoria '\(category)':\n\(error.localizedDescription)")
+        }
+        
+        return 0
+    }
+    
+    func updatePost(post: PostUpdate, id: Int) async {
+        isLoading = true
+        do {
+            let _ = try await NetworkManager.shared.updatePost(post: post, id: id)
+        } catch {
+            print("Error al actualizar el reporte: \(error.localizedDescription)")
         }
         isLoading = false
     }
